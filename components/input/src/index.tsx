8e915bff1c1d063a26e68bf77cfafdf8e5aba50c
Maturen, Aaron T <aaron.maturen@cengage.com>
2020-11-24 12:07:25 -0500

    fix(input): lazy ts linting

--- components/input/src/index.tsx
+++ components/input/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export const Input = ({name, label, ...props}) => {
+export const Input = ({name:string, label:string, ...props}) => {
   <>
     <label for={name}>`${label}`</label>
     return <input name={name} {...props}/>;
