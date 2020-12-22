fc1d2f2840ff3f3df93f3c61ff8b68a24b28252f
Aaron Maturen <aaron.maturen@cengage.com>
2020-12-22 16:50:33 -0500

    fix(button, react): add button to react lock

--- components/input/src/index.tsx
+++ components/input/src/index.tsx
@@ -4,7 +4,9 @@ export interface InputProps extends React.HTMLAttributes<HTMLInputElement> {
   label: string;
 }
 
-export const Input: React.FC<InputProps> = ({label, ...props}) => <>
-  <label htmlFor={props.id}>{label}</label>
-  <input {...props}/>;
-</>;
+export const Input: React.FC<InputProps> = ({ label, ...props }) => (
+  <>
+    <label htmlFor={props.id}>{label}</label>
+    <input {...props} />;
+  </>
+);
