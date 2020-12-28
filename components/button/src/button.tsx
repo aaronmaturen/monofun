c55879143d7b2302aeb2b325fbfc584a2589657c
Aaron Maturen <aaron.maturen@cengage.com>
2020-12-28 13:29:37 -0500

    chore(react): temporarily remove react

--- components/button/src/button.tsx
+++ components/button/src/button.tsx
@@ -1,3 +1,3 @@
 import * as React from 'react';
 export interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {}
-export const Button: React.FC<ButtonProps> = (props) => <button {...props} />;
+export const Button: React.FC<ButtonProps> = props => <button {...props} />;
